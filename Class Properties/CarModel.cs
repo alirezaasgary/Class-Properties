6e0027fe978a7a60c032327dac5bcb1495e44b49
alireza asgary <asgary_alireza@hotmail.com>
2023-01-20 16:09:38 +0300

    Add project files.

---
+++ Class Properties/CarModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Properties
+{
+    public class CarModel
+    {
+        private string _name;
+        public string Model;
+        private string _color;
+
+        List<string> validColor = new List<string>() { "red", "blue", "black" };
+
+        List<string> validCar = new List<string>() { "pride", "samand", "vanet" };
+
+
+        public CarModel(string name, string model, string color)
+        {
+            Name = name;
+            Model = model;
+            Color = color;
+        }
+
+        public string Color
+        {
+            get
+            {
+                return _color;
+            }
+
+            set
+            {
+                if(validColor.Contains(value)) 
+                
+                {
+                    _color= value;
+                }
+                else
+                {
+                    _color = "ND";
+                }
+            }
+
+        }
+
+        public string Name {
+            get
+            {
+                return _name;
+            } 
+            
+            set
+            {
+                if (validCar.Contains(value))
+                {
+                    _name= value;
+                }
+                else
+                {
+                    _name = "ND";
+                }
+            }
+        
+        }
+
+
+    }
+}
---
+++ Class Properties/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Class_Properties;
+
+
+CarModel car1 = new CarModel("pride", "1900", "green");
+CarModel car2 = new CarModel("vbjanet", "2000", "blue");
+
+Console.WriteLine($"{car1.Name} - {car1.Color}");
+Console.WriteLine($"{car2.Name} - {car2.Color}");
+
+
+Console.ReadKey();
